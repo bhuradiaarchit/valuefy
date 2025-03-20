46ead83486926d93e12ecfba49ef6db9c066bbf8
unknown <arinzingade29@gmail.com>
2025-03-20 15:42:55 +0530

    feat: entrypoint fixed

--- nse_scraper/entrypoint.sh
+++ nse_scraper/entrypoint.sh
@@ -4,7 +4,7 @@ export TZ=Asia/Kolkata
 
 # Define the cron job
 CRON_JOB="
-00 18 * * * /usr/local/bin/python3 /app/main.py >> /var/log/scrape.log 2>&1
+30 14 * * * /usr/local/bin/python3 /app/main.py >> /var/log/scrape.log 2>&1
 "
 
 # Add cron job to the crontab
